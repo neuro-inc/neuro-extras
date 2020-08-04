573266a9fdd9f52a36cec984bb734591cd9fa302
Roman Skurikhin <romansku135@gmail.com>
2020-08-04 15:26:04 +0300

    Add upload and download commands

--- tests/e2e/test_main.py
+++ tests/e2e/test_main.py
@@ -11,6 +11,7 @@ from typing import Callable, Iterator, List
 from unittest import mock
 
 import pytest
+import toml
 import yaml
 from _pytest.capture import CaptureFixture
 from neuromation.cli.const import EX_OK
@@ -471,3 +472,60 @@ def test_seldon_generate_deployment_custom(cli_runner: CLIRunner) -> None:
             ]
         },
     }
+
+
+@pytest.fixture()
+def remote_project_dir(project_dir: Path) -> Path:
+    local_conf = project_dir / ".neuro.toml"
+    remote_project_dir = "e2e-test-remote-dir"
+    local_conf.write_text(
+        toml.dumps({"extra": {"remote-project-dir": remote_project_dir}})
+    )
+    return Path(remote_project_dir)
+
+
+def test_upload_download_single_file(
+    project_dir: Path, remote_project_dir: Path, cli_runner: CLIRunner
+) -> None:
+    test_file_name = "test.txt"
+    test_file_content = "Testing"
+
+    file = project_dir / test_file_name
+    file.write_text(test_file_content)
+    result = cli_runner(["neuro-extras", "upload", test_file_name])
+    assert result.returncode == 0, result
+    file.unlink()
+    # Redownload file
+    result = cli_runner(["neuro-extras", "download", test_file_name])
+    assert result.returncode == 0, result
+    file = project_dir / test_file_name
+    assert file.read_text() == test_file_content
+
+
+def test_upload_download_subdir(
+    project_dir: Path, remote_project_dir: Path, cli_runner: CLIRunner
+) -> None:
+    subdir_name = "sub"
+    test_file_name = "test.txt"
+    test_file_content = "Testing"
+
+    file_in_root = project_dir / test_file_name
+    file_in_root.write_text(test_file_content)
+    subdir = project_dir / subdir_name
+    subdir.mkdir()
+    file_in_subdir = project_dir / subdir_name / test_file_name
+
+    file_in_subdir.write_text(test_file_content)
+
+    result = cli_runner(["neuro-extras", "upload", subdir_name])
+    assert result.returncode == 0, result
+    file_in_root.unlink()
+    file_in_subdir.unlink()
+    subdir.rmdir()
+    # Redownload folder
+    result = cli_runner(["neuro-extras", "download", subdir_name])
+    assert result.returncode == 0, result
+    file_in_root = project_dir / test_file_name
+    assert not file_in_root.exists(), "File in project root should not be downloaded"
+    file_in_subdir = project_dir / subdir_name / test_file_name
+    assert file_in_subdir.read_text() == test_file_content
