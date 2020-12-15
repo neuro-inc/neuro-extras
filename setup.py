3a10faa84730d4433b204083f3ebfeea2dccabd3
Alexey Naiden <17383+anayden@users.noreply.github.com>
2020-12-15 14:30:47 +0300

    remove Neuro SDK dependency (#177)

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         "neuro-cli==20.12.14",
-        "neuro-sdk==20.12.14",
         'dataclasses>=0.7; python_version<"3.7"',
         "click>=7.0",
         "toml>=0.10.0",
