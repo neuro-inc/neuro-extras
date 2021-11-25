1b2438bb6974e1aac67c91accf38ba889a88afb1
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-11-25 16:09:14 +0200

    Bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url="https://github.com/neuro-inc/neuro-extras",
     packages=find_packages(),
     install_requires=[
-        "neuro-cli>=21.11.3",
+        "neuro-cli>=21.11.4",
         "click>=8.0",
         "toml>=0.10.0",
         "pyyaml>=3.0",
