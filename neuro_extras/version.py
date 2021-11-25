22aacaea63ad2ea85c4bdc330c53c2543f77b9f5
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-11-25 17:08:38 +0200

    Bump to 21.11.5

--- neuro_extras/version.py
+++ neuro_extras/version.py
@@ -1,3 +1,1 @@
-# Note: version MUST be in format '20.10.23a6' because it defines the docker image
-# that we use, see tags in https://hub.docker.com/r/neuromation/neuro-extras/tags
-__version__ = "21.11.4"
+__version__ = "21.11.5"
