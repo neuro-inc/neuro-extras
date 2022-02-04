8869c5c5dba5bd8e59b9011a433941b0e1e23bf4
Alexey Naiden <alexey.nayden@gmail.com>
2022-02-04 15:48:34 +0300

    Address code review comments

--- tests/e2e/test_image.py
+++ tests/e2e/test_image.py
@@ -457,7 +457,7 @@ def test_image_local_build(cli_runner: CLIRunner) -> None:
     random_file_to_disable_layer_caching = gen_random_file(dockerfile_path.parent)
 
     if sys.platform == "win32":
-        base_image = "mcr.microsoft.com/windows/nanoserver:2022"
+        base_image = "mcr.microsoft.com/windows/nanoserver:latest"
     else:
         base_image = "ghcr.io/neuro-inc/alpine:latest"
 
