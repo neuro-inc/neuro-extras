a7bea0d93a8775553fcebc9227f47e44bbf7012a
Yevhenii Semendiak <semendyak@gmail.com>
2024-08-09 03:39:12 +0300

    Publish neuro-extras

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import re
 from setuptools import find_packages, setup
 
 
+DIST_NAME = "apolo-extras"
+
 with open("apolo_extras/version.py") as f:
     txt = f.read()
     try:
@@ -12,20 +14,20 @@ with open("apolo_extras/version.py") as f:
 
 
 setup(
-    name="apolo-extras",
+    name=DIST_NAME,
     version=version,
     python_requires=">=3.8.0",
     url="https://github.com/neuro-inc/neuro-extras",
     packages=find_packages(),
     install_requires=[
-        "apolo-cli>=24.7.1",
+        "apolo-cli>=24.8.1",
         "click>=8.0",
         "toml>=0.10.0",
         "pyyaml>=3.0",
     ],
     entry_points={
-        "console_scripts": ["apolo-extras=apolo_extras:main"],
-        "apolo_api": ["apolo-extras=apolo_extras:setup_plugin"],
+        "console_scripts": [f"{DIST_NAME}=apolo_extras:main"],
+        "apolo_api": [f"{DIST_NAME}=apolo_extras:setup_plugin"],
     },
     zip_safe=False,
     include_package_data=True,
