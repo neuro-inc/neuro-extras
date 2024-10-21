004ced650bded401dd562052dd23079d7df6c27a
Andrew Svetlov <andrew.svetlov@gmail.com>
2024-10-21 18:41:37 +0200

    Release 24.10.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url="https://github.com/neuro-inc/neuro-extras",
     packages=find_packages(),
     install_requires=[
-        "apolo-cli>=24.9.0",
+        "apolo-cli>=24.10.1",
         "click>=8.0",
         "toml>=0.10.0",
         "pyyaml>=3.0",
